962e49c3f1c25b6f8005dbb84de92888c09190bb
Sword <330132662@qq.com>
2025-05-12 18:08:35 +0800

    用户端 新增功能用的源码  懒得再开个版本库了 放一起

---
+++ uni/pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		<AddressParse @addressConfirm="addressConfirm"></AddressParse>
+	</view>
+</template>
+<script>
+	import AddressParse from '@/components/xghc-addrress/userAddress.vue'
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		components:{
+			AddressParse
+		},
+		onLoad(){
+			
+		},
+		methods: {
+			addressConfirm(data){
+				console.log(data);
+			}
+		},
+	}
+</script>
+<style>
+	
+</style>
